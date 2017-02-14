c28276fd4d06ed11d87da4cdcb349c63f84d1b4a
Mostafa Barmshory <mostafa.barmshory@dpq.co.ir>
2017-02-15 03:15:15 +0330

    update

--- index.php
+++ index.php
@@ -1,32 +1,82 @@
 <?php
-/*
-=====================================================
-PHP Setup Wizard Script - by VLD Interactive Inc.
-----------------------------------------------------
-http://www.phpsetupwizard.com/
-http://www.vldinteractive.com/
------------------------------------------------------
-Copyright (c) 2015 VLD Interactive Inc.
-=====================================================
-THIS IS COPYRIGHTED SOFTWARE
-PLEASE READ THE LICENSE AGREEMENT
-http://www.phpsetupwizard.com/license/
-=====================================================
-*/
+
 
 ini_set('display_errors', 'on');
 error_reporting(E_ALL);
-$base_path = '/home/maso/git/pluf/demo/vendor/pluf/installer/';
 
+require_once('install/lib/WebApplicationInstaller.class.php');
 
-// $base_path = str_replace('\\', '/', realpath(dirname(__FILE__))).'/';
-$virtual_path = str_replace('\\', '/', 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF'])).'/';
-define('BASE_PATH', $base_path);
-define('VIRTUAL_PATH', $virtual_path);
+WAI::setLanguage('de_DE');
 
-include BASE_PATH . 'includes/core/wizard.php';
-include BASE_PATH . 'includes/wizard.php';
+WAI::setStyle('install/custom/css/silverstripe.css');
+WAI::setTitle('SilverStripe CMS Installation');
+WAI::setLogo('install/custom/images/silverstripe.png');
 
-$wizard = new phpSetupWizard();
+WAI::text('== Welcome to SilverStripe ==');
+WAI::text('Thanks for choosing to use SilverStripe! Please follow the instructions below to get SilverStripe installed.');
 
-$wizard->run();
+WAI::dropdownField(
+	'template',
+	'Template to install:',
+	array(
+		'mysite'
+	),
+	'mysite',
+	'You can change the template or download another from the SilverStripe website after installation.'
+);
+
+WAI::checkboxField('send_information', 'Send information on my webserver to SilverStripe (this is only version information, used for statistical purposes)', true, true);
+
+WAI::validateCustom('CheckPreviousInstallstion');
+
+WAI::requestDatabaseSettings(WAI::DB_MySQL, array('min_version' => 4));
+
+WAI::text('=== SilverStripe Administration Account ===');
+
+WAI::textField(
+	'admin_email',
+	'Administrator email:',
+	'',
+	'We will set up 1 administrator account for you automatically. Enter the email address and password. If you\'d rather log-in with a username instead of an email address, enter that instead.'
+);
+WAI::textField('admin_password', 'Administrator password:');
+WAI::textField('admin_first_name', 'Administrator first name:');
+WAI::textField('admin_surname', 'Administrator surname:');
+
+WAI::validateCustom('CreateAdminAccount');
+
+WAI::text('=== Development Servers ===');
+
+WAI::textareaField(
+	'development_servers',
+	'Development servers:',
+	"localhost\n127.0.0.1",
+	'SilverStripe allows you to run a site in [http://doc.silverstripe.com/doku.php?id=devmode development mode]. This shows all error messages in the web browser instead of emailing them to the administrator, and allows the database to be built without logging in as administrator. Please enter the host/domain names for servers you will be using for development.'
+);
+
+WAI::requirePhpConfiguration(WAI::PHP5);
+WAI::requirePhpConfiguration(WAI::GD2);
+WAI::requirePhpConfiguration(WAI::XML);
+WAI::requirePhpConfiguration(WAI::DB_MySQL);
+WAI::requirePhpConfiguration(WAI::MEMORY_ALLOCATED, array('min' => '32MB'));
+WAI::requirePhpConfiguration(WAI::NOT_DEFINED_CLASSES, array(
+	'Query',
+	'HTTPResponse'
+));
+
+WAI::requirePermission(WAI::FOLDER_EXISTS, WAI::SERVER_ROOT);
+WAI::requirePermission(WAI::FOLDER_EXISTS, 'mysite/');
+WAI::requirePermission(WAI::FOLDER_EXISTS, 'sapphire/');
+WAI::requirePermission(WAI::FOLDER_EXISTS, 'cms/');
+WAI::requirePermission(WAI::FOLDER_EXISTS, 'jsparty/');
+WAI::requirePermission(WAI::FILE_WRITE, '.htaccess');
+WAI::requirePermission(WAI::FILE_WRITE, 'mysite/_config.php');
+WAI::requirePermission(WAI::FOLDER_WRITE, 'assets/');
+WAI::requirePermission(WAI::FOLDER_WRITE, WAI::TEMPORARY_FOLDER);
+
+WAI::requireWebserverConfiguration(WAI::SERVER_SOFTWARE, array('min_version' => 'Apache/2'));
+WAI::requireWebserverConfiguration(WAI::MOD_REWRITE);
+
+WAI::dispatch();
+
+?>
---
+++ install/custom/CheckPreviousInstallstion.php
@@ -0,0 +1,47 @@
+<?php
+
+class CheckPreviousInstallstion implements IWebApplicationInstaller_Script
+{
+    /**
+     * @see IWebApplicationInstaller_Script::run()
+     */
+    public function run()
+    {
+        $alreadyInstalled = false;
+		if(file_exists('mysite/_config.php'))
+		{
+			// Find the $database variable in the relevant config file without having to execute the config file
+			if(preg_match('/\\\$database\s*=\s*[^\n\r]+[\n\r]/', file_get_contents('mysite/_config.php'), $parts))
+			{
+				eval($parts[0]);
+				if($database)
+				{
+					$alreadyInstalled = true;
+					// Assume that if $databaseConfig is defined in mysite/_config.php, then a non-environment-based installation has
+					// already gone ahead
+				}
+				else if(preg_match('/\\\$databaseConfig\s*=\s*[^\n\r]+[\n\r]/', file_get_contents('mysite/_config.php'), $parts))
+				{
+					$alreadyInstalled = true;
+				}
+			}
+		}
+		
+		if($alreadyInstalled)
+		{
+			WAI::warningMsg("'''Note:''' It seems as though SilverStripe is already installed here. If you ask me to install, I will overwrite the '''.htaccess''' and '''mysite/_config.php''' files.");
+		}
+		
+		return true;
+    }
+
+    /**
+     * @see IWebApplicationInstaller_CustomScript::getErrorMsg()
+     */
+    public function getErrorMsg()
+    {
+        return '';
+    }
+}
+
+?>
